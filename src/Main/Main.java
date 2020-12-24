21c29dc8049f4cb34c0a76ce603a9daa869d440b
Mehmet Sökmen <sokmen4083@gmail.com>
2020-12-24 14:00:53 +0100

    create studentnumber has done

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -25,15 +25,38 @@ public class Main {
                 System.out.println("Please enter a value between 6 and 10.  " + i + ". student age: ");
                 student.setAge(scanner.nextInt());
             }
-            String twoLettersOfFirstName = student.getFirstName().substring(0, 2);
-            String lastThreeLetterOfLastName = student.getLastName().substring(students.length);
-            int age = student.getAge();
-            String studentNumber = student.setStudentNumber();
-            studentNumber = twoLettersOfFirstName + lastThreeLetterOfLastName + age;
-            System.out.println(i + ". student's number is " + studentNumber);
+            if (student.getLastName().length() >= 3){
+                String twoLettersOfFirstName = student.getFirstName().substring(0, 2);
+                int age = student.getAge();
+                String studentNumber = student.setStudentNumber();
+                String lastThreeLetterOfLastName = student.getLastName().substring(student.getLastName().length()-3,student.getLastName().length());
+                studentNumber = twoLettersOfFirstName + lastThreeLetterOfLastName + age;
+                System.out.println(student.getFirstName() + " " + student.getLastName() + "'s number is " + studentNumber);
+            }else if(student.getLastName().length() < 3){
+                System.out.println(" Enter lastname greater than 3 letters.");
+                System.out.println("Enter " + i + ". student lastname: ");
+                student.setLastName(scanner.next());
+                String twoLettersOfFirstName = student.getFirstName().substring(0, 2);
+                int age = student.getAge();
+                String studentNumber = student.setStudentNumber();
+                String lastThreeLetterOfLastName = student.getLastName().substring(student.getLastName().length()-3,student.getLastName().length());
+                studentNumber = twoLettersOfFirstName + lastThreeLetterOfLastName + age;
+                System.out.println(student.getFirstName() + " " + student.getLastName() + "'s number is " + studentNumber);
+
+            }
             students[i] = student;
         }
         return students;
     }
-
+    private static Class[] addTheStudentsInClass(int n){
+        Class[] students = new Class[n];
+        Scanner scanner = new Scanner(System.in);
+        for(int i = 1; i < n+1; i++){
+           Student student = new Student();
+           student.setFirstName(scanner.nextLine());
+           student.setLastName(scanner.nextLine());
+           student.setAge(scanner.nextInt());
+       }
+        return students;
+    }
 }
